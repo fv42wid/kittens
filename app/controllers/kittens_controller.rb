691905bf5e1a50c98e7099038e67ec68211e795f
Frank Vinci <fv42wid@yahoo.com>
2016-10-05 14:03:22 +0000

    update kitten, #7 next

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -25,6 +25,15 @@ class KittensController < ApplicationController
     @kitten = Kitten.find(params[:id])
   end
 
+  def update
+    @kitten = Kitten.find(params[:id])
+    if @kitten.update(kitten_params)
+      redirect_to @kitten
+    else
+      redirect_to edit_kitten_path(@kitten)
+    end
+  end
+
   private
     def kitten_params
       params.require(:kitten).permit(:name, :age, :cuteness, :softness)
