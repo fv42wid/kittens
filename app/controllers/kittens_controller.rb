c8831c6e976c80ceda847b74b5d601a50082a1f9
Frank Vinci <fv42wid@yahoo.com>
2016-10-05 13:58:23 +0000

    create and show kitten, #6 update next

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,6 +1,32 @@
 class KittensController < ApplicationController
 
   def index
-
+    @kittens = Kitten.all
   end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+    if @kitten.save
+      redirect_to @kitten
+    else
+      redirect_to new_kitten_path
+    end
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  private
+    def kitten_params
+      params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+    end
 end
